6c1ef22dcef736eab925bdf5ba8cbbb53b15410a
Shrinkhal <97280075+Shrinkhal01@users.noreply.github.com>
2024-06-29 06:40:14 +0530

    Update app.py
    just to make it work

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import json
-import os
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 class OllamaChatbot:
-    def __init__(self, base_url, model, history_file):
+    def __init__(self, base_url, model):
         self.base_url = base_url
         self.model = model
         self.chat_history = []
         self.system_prompt = ""
         self.keep_alive = "10m"
-        self.history_file = history_file
-        self.load_history()
-
-    def load_history(self):
-        if os.path.exists(self.history_file):
-            with open(self.history_file, 'r') as file:
-                self.chat_history = json.load(file)
-        else:
-            self.chat_history = []
-
-    def save_history(self):
-        with open(self.history_file, 'w') as file:
-            json.dump(self.chat_history, file)
 
     def generate_completion(self, prompt, system_message="", stream=True):
         headers = {"Content-Type": "application/json"}
@@ -64,7 +50,6 @@ class OllamaChatbot:
             for message in self.generate_completion(prompt, self.system_prompt):
                 full_message += message
             self.chat_history.append({"role": "bot", "content": full_message})
-            self.save_history()
             return full_message
         except requests.exceptions.RequestException as e:
             print(f"\nError: {e}")
@@ -79,20 +64,9 @@ def handle_chat():
     user_input = request.form['user_input']
     base_url = "http://localhost:11434"
     model = "llama2:latest"
-    history_file = 'nfs/chat_history.txt'
-    chatbot = OllamaChatbot(base_url, model, history_file)
+    chatbot = OllamaChatbot(base_url, model)
     response = chatbot.chat(user_input)
     return jsonify({'response': response})
-
-@app.route('/history', methods=['GET'])
-def get_history():
-    history_file = 'nfs/chat_history.txt'
-    if os.path.exists(history_file):
-        with open(history_file, 'r') as file:
-            chat_history = json.load(file)
-        chat_history_display = [f"{entry['role']}: {entry['content']}" for entry in chat_history]
-        return jsonify(chat_history_display)
-    return jsonify([])
 
 if __name__ == '__main__':
     app.run(debug=True)
